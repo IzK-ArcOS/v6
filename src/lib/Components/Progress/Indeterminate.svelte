113fc4c60a0865eb3c3faacb81bb385cb49b16e2
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-02 11:35:58 +0100

    Update indeterminate progress bar

--- src/lib/Components/Progress/Indeterminate.svelte
+++ src/lib/Components/Progress/Indeterminate.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   export let width = 250;
+  export let active = true;
 </script>
 
 <div class="progressbar" style="width: {width}px;">
-  <div class="inc" />
-  <div class="dec" />
+  {#if active}
+    <div class="inc" />
+    <div class="dec" />
+  {/if}
 </div>

632934ed4e8c650bd3621b12b3e4e9d8c1799501
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-06 09:21:25 +0100

    Add some more icons

--- src/ts/images/general.ts
+++ src/ts/images/general.ts
@@ -3,3 +3,5 @@ export { default as SecureIcon } from "$assets/general/secure.svg";
 export { default as BugReportIcon } from "$assets/general/bugreports.svg";
 export { default as HomeIcon } from "$assets/general/home.svg";
 export { default as ConnectIcon } from "$assets/general/connect.svg";
+export { default as WarningIcon } from "$assets/general/warning.svg";
+export { default as FlagIcon } from "$assets/general/flag.svg";

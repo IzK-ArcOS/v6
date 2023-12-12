2349957b5e180399b39363583fd8eb2c2811f441
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-12 20:19:02 +0100

    Add Shell submodule

--- src/ts/stores/apps/builtins.ts
+++ src/ts/stores/apps/builtins.ts
@@ -1,8 +1,10 @@
+import { ArcShell } from "$state/Shell/ts/app";
 import { App } from "$types/app";
 import { ArcTermApp as ArcTerm } from "../../../apps/ArcTerm/ts/app";
 import { desktopWallpaper } from "../../../apps/Wallpaper/ts/app";
 
 export const builtinApps: Record<string, App> = {
   desktopWallpaper,
-  ArcTerm
+  ArcShell,
+  ArcTerm,
 }

fefc644a3548100222f100051598cd932348ef84
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-12 20:57:45 +0100

    Document icon now reflects the release mode

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import StateRenderer from "$lib/StateRenderer.svelte";
+  import { Logo } from "$ts/branding";
   import { getBuild, getMode } from "$ts/metadata";
   import { PrimaryState } from "$ts/states";
   import { CRASHING } from "$ts/stores/crash";
@@ -16,6 +17,10 @@
     render = true;
   });
 </script>
+
+<svelte:head>
+  <link rel="icon" href={Logo()} />
+</svelte:head>
 
 {#if render}
   <div class="app theme-dark fullscreen" class:gray={$CRASHING}>

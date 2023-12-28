373871e1a6e66a053cda94dd5a3e5e95899ad5f7
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-28 12:28:34 +0100

    Add glowing logo

--- src/ts/images/branding.ts
+++ src/ts/images/branding.ts
@@ -1,5 +1,7 @@
 export { default as DevelopmentLogo } from "$assets/branding/devIcon.svg";
 export { default as EsrLogo } from "$assets/branding/esr.png";
+export { default as GlowingLogo } from "$assets/branding/glowing.png";
 export { default as RcLogo } from "$assets/branding/rc.png";
 export { default as ReleaseLogo } from "$assets/branding/systemIcon.svg";
 export { default as UnstableLogo } from "$assets/branding/unstIcon.png";
+

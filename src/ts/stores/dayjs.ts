76f6c2ae3b23745d3cdac9b8637758025972b9bc
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-31 09:23:10 +0100

    Add RelativeTimeMod

---
+++ src/ts/stores/dayjs.ts
@@ -0,0 +1,17 @@
+export const RelativeTimeMod = {
+  relativeTime: {
+    future: "in %s",
+    past: "%s ago",
+    s: "1 sec",
+    m: "1 min",
+    mm: "%d min",
+    h: "an hr",
+    hh: "%d hrs",
+    d: "1 day",
+    dd: "%d days",
+    M: "1 month",
+    MM: "%d months",
+    y: "1 yr",
+    yy: "%d yrs",
+  },
+}

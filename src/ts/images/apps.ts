52a0923d45bb493251200611f329983c050f478f
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-01 19:59:36 +0100

    Add LoggerApp icon and import

--- src/ts/images/apps.ts
+++ src/ts/images/apps.ts
@@ -2,3 +2,4 @@ export { default as ArcTermIcon } from "$assets/apps/arcterm.svg";
 export { default as DefaultIcon } from "$assets/apps/default.svg";
 export { default as SettingsIcon } from "$assets/apps/settings.svg";
 export { default as AppInfoIcon } from "$assets/apps/appinfo.svg";
+export { default as LoggerIcon } from "$assets/apps/logger.svg";
--- src/ts/stores/apps/builtins.ts
+++ src/ts/stores/apps/builtins.ts
@@ -3,6 +3,7 @@ import { ArcTermApp as ArcTerm } from "$apps/ArcTerm/ts/app";
 import { SettingsApp } from "$apps/Settings/ts/app";
 import { ArcShell } from "$apps/Shell/ts/app";
 import { desktopWallpaper } from "$apps/Wallpaper/ts/app";
+import { LoggerApp } from "$apps/v6-LoggerApp/ts/app";
 import { App } from "$types/app";
 
 export const builtinApps: Record<string, App> = {
@@ -10,5 +11,6 @@ export const builtinApps: Record<string, App> = {
   ArcShell,
   ArcTerm,
   SettingsApp,
-  AppInfo
+  AppInfo,
+  LoggerApp
 }

c926fa79583e24c540bd8f78f8be22c028555243
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-05 15:05:33 +0100

    Update ErrorDialog

--- src/apps/ErrorDialog/ts/runtime.ts
+++ src/apps/ErrorDialog/ts/runtime.ts
@@ -1,5 +1,6 @@
 import { AppRuntime } from "$ts/apps/runtime";
 import { WarningIcon } from "$ts/images/general";
+import { sendNotification } from "$ts/notif";
 import { Process } from "$ts/process";
 import type { App, AppMutator } from "$types/app";
 import { ErrorDialog } from "$types/error";
@@ -8,9 +9,22 @@ export class Runtime extends AppRuntime {
   constructor(app: App, mutator: AppMutator, process: Process) {
     super(app, mutator, process);
 
+    function stop() {
+      process.handler.kill(process.pid);
+
+      sendNotification({
+        title: "Can't open Error",
+        message: `No valid data was specified to use in the dialog. Aborting.`,
+        image: WarningIcon,
+        timeout: 3000,
+      })
+    }
+
     const data: ErrorDialog = process.args[0];
 
-    if (!data) return;
+    if (!data) {
+      stop(); return
+    }
 
     mutator.update((v) => { // Adapt the window properties to the error dialog's data
       v.metadata.name = data.title;
--- src/ts/stores/apps/builtins.ts
+++ src/ts/stores/apps/builtins.ts
@@ -1,9 +1,10 @@
 import { AppInfo } from "$apps/AppInfo/ts/app";
 import { ArcTermApp as ArcTerm } from "$apps/ArcTerm/ts/app";
+import { ErrorDialog } from "$apps/ErrorDialog/ts/app";
+import { LoggerApp } from "$apps/LoggerApp/ts/app";
 import { SettingsApp } from "$apps/Settings/ts/app";
 import { ArcShell } from "$apps/Shell/ts/app";
 import { desktopWallpaper } from "$apps/Wallpaper/ts/app";
-import { LoggerApp } from "$apps/LoggerApp/ts/app";
 import { App } from "$types/app";
 
 export const builtinApps: Record<string, App> = {
@@ -12,5 +13,6 @@ export const builtinApps: Record<string, App> = {
   ArcTerm,
   SettingsApp,
   AppInfo,
-  LoggerApp
+  LoggerApp,
+  ErrorDialog
 }

bb6577caeb7cc6195ed4ba8bf13c610b671fb06e
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-10 17:15:06 +0100

    Add icon to Qlorb

--- src/ts/images/apps.ts
+++ src/ts/images/apps.ts
@@ -3,5 +3,6 @@ export { default as ArcTermIcon } from "$assets/apps/arcterm.svg";
 export { default as DefaultIcon } from "$assets/apps/default.svg";
 export { default as LoggerIcon } from "$assets/apps/logger.svg";
 export { default as ProcessManagerIcon } from "$assets/apps/processmanager.svg";
+export { default as QlorbIcon } from "$assets/apps/qlorb.svg";
 export { default as SettingsIcon } from "$assets/apps/settings.svg";
 

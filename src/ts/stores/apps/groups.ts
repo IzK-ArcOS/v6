5237e2af6095a732363ddb2e9fa815e329582622
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-19 21:45:37 +0100

    Update app groups

--- src/ts/stores/apps/groups.ts
+++ src/ts/stores/apps/groups.ts
@@ -2,31 +2,31 @@ import { AppGroupStore } from "$types/groups";
 
 export const appGroups: AppGroupStore = {
   communication: {
-    name: "Communication",
+    name: "Communication", // Communication with other people (messaging)
     id: "communication",
   },
   coreApps: {
-    name: "Core Apps",
+    name: "Core Apps", // `App.metadata.core` enabled apps (wallpaper, shell)
     id: "coreApps",
   },
   entertainment: {
-    name: "Entertainment",
+    name: "Entertainment", // Games, media player, etc
     id: "entertainment",
   },
   utilities: {
-    name: "Utilities",
+    name: "Utilities", // Everyday office utilities
     id: "utilities",
   },
   support: {
-    name: "Help and Support",
+    name: "Help and Support", // Tools used for help and support
     id: "support",
   },
   systemTools: {
-    name: "System Tools",
+    name: "System Tools", // Tools used to manage ArcOS
     id: "systemTools",
   },
   internal: {
-    name: "Internal Tools",
+    name: "%internal%", // Hidden applications only called programmatically
     id: "internal",
   },
 };

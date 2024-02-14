df3ab09af4a9d8fa255e5c79b808ec41ca0af37b
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-14 18:26:02 +0100

    Add provided user store in `getUserPfp`

--- src/ts/server/user/pfp.ts
+++ src/ts/server/user/pfp.ts
@@ -1,18 +1,19 @@
 import { ProfilePictures, getProfilePicture } from "$ts/stores/pfp";
 import { ConnectedServer } from "$ts/stores/server";
 import { Params } from "$types/server";
+import { AllUsers } from "$types/user";
 import { getUsers } from "./get";
 
 const pfpCache: Params = {};
 
-export async function getUserPfp(username: string, fallback = ""): Promise<string> {
+export async function getUserPfp(username: string, fallback = "", store?: AllUsers): Promise<string> {
   const server = ConnectedServer.get();
 
   if (!server) return fallback || ProfilePictures.def;
 
   if (pfpCache[username]) return getProfilePicture(pfpCache[username]);
 
-  const users = await getUsers();
+  const users = store || (await getUsers());
 
   const user = users[username];
 

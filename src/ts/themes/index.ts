0057273d087c59658aa2b0271bab2914cc93b1be
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-15 16:55:34 +0100

    Make sure missing userdata bools don't corrupt the ArcTheme

--- src/ts/themes/index.ts
+++ src/ts/themes/index.ts
@@ -19,21 +19,21 @@ export function loadTheme(context: UserTheme) {
   }
 
   UserDataStore.update((udata) => {
-    udata.sh.anim = context.anim;
-    udata.sh.noGlass = context.noGlass;
-    udata.sh.desktop.sharp = context.sharp;
+    udata.sh.anim = !!context.anim;
+    udata.sh.noGlass = !!context.noGlass;
+    udata.sh.desktop.sharp = !!context.sharp;
     udata.sh.desktop.theme = context.theme;
     udata.sh.desktop.wallpaper = context.wallpaper;
     udata.sh.desktop.accent = context.accent;
-    udata.sh.taskbar.docked = context.docked;
-    udata.sh.taskbar.centered = context.taskbarCentered;
-    udata.sh.taskbar.labels = context.taskbarLabels;
+    udata.sh.taskbar.docked = !!context.docked;
+    udata.sh.taskbar.centered = !!context.taskbarCentered;
+    udata.sh.taskbar.labels = !!context.taskbarLabels;
     udata.sh.taskbar.pos = context.taskbarPosition;
-    udata.sh.start.small = context.smallStart;
+    udata.sh.start.small = !!context.smallStart;
     udata.sh.window.buttons = context.titleButtons;
-    udata.sh.window.bigtb = context.titlebarLarge;
-    udata.sh.window.lefttb = context.titlebarLeft;
-    udata.sh.taskbar.colored = context.taskbarColored;
+    udata.sh.window.bigtb = !!context.titlebarLarge;
+    udata.sh.window.lefttb = !!context.titlebarLeft;
+    udata.sh.taskbar.colored = !!context.taskbarColored;
     udata.sh.window.centertb = !!context.titlebarCentered;
     udata.sh.taskbar.isLauncher = !!context.isLauncher;
 

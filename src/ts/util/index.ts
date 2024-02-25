64bda5a5b93ee6a8739a24e44ddb8a25632c3827
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-25 22:05:08 +0100

    Add `Repeat`, `Longest` utils

--- src/ts/util/index.ts
+++ src/ts/util/index.ts
@@ -5,3 +5,17 @@ export const Plural = (s: string, x: number) => `${s}${x == 1 ? "" : "s"}`;
 export function CountInstances(input: string, search: string) {
   return input.split(search).length;
 }
+
+export function Repeat(char: string, amount: number) {
+  return char.repeat(amount);
+}
+
+export function Longest(...strings: string[]): number {
+  let length = 0;
+
+  for (const string of strings) {
+    if (string.length > length) length = string.length;
+  }
+
+  return length;
+}
